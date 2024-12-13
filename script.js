2a9c49c648d57b62fa2e0b60f3b5627307568298
federicodececco <federicodececco1@gmail.com>
2024-12-13 16:27:35 +0100

    add:script and some style

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 const teamMembers = [
   {
-    name: "Marco Bianchi",
-    role: "Designer",
-    email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    name: 'Marco Bianchi',
+    role: 'Designer',
+    email: 'marcobianchi@team.com',
+    img: 'img/male1.png',
   },
   {
-    name: "Laura Rossi",
-    role: "Front-end Developer",
-    email: "laurarossi@team.com",
-    img: "img/female1.png"
+    name: 'Laura Rossi',
+    role: 'Front-end Developer',
+    email: 'laurarossi@team.com',
+    img: 'img/female1.png',
   },
   {
-    name: "Giorgio Verdi",
-    role: "Back-end Developer",
-    email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    name: 'Giorgio Verdi',
+    role: 'Back-end Developer',
+    email: 'giorgioverdi@team.com',
+    img: 'img/male2.png',
   },
   {
-    name: "Marta Ipsum",
-    role: "SEO Specialist",
-    email: "martarossi@team.com",
-    img: "img/female2.png"
+    name: 'Marta Ipsum',
+    role: 'SEO Specialist',
+    email: 'martarossi@team.com',
+    img: 'img/female2.png',
   },
   {
-    name: "Roberto Lorem",
-    role: "SEO Specialist",
-    email: "robertolorem@team.com",
-    img: "img/male3.png"
+    name: 'Roberto Lorem',
+    role: 'SEO Specialist',
+    email: 'robertolorem@team.com',
+    img: 'img/male3.png',
   },
   {
-    name: "Daniela Amet",
-    role: "Analyst",
-    email: "danielaamet@team.com",
-    img: "img/female3.png"
-  }
-];
+    name: 'Daniela Amet',
+    role: 'Analyst',
+    email: 'danielaamet@team.com',
+    img: 'img/female3.png',
+  },
+]
+const cardSection = document.getElementById('teamCard')
+let sectionArray = ''
+const createCard = struct => {
+  const { name, role, email, img } = struct
+  const card = `<div class="flex bg-black basis-[calc(33.333%-1rem)] ">
+  <div class="basis-1/3">
+    <img src="./${img}" alt="" />
+  </div>
+  <div class="p-3 text-white">
+    <h4 class="text-2xl font-semibold">${name}</h4>
+    <p class="pt-2">${role}</p>
+    <p class="email pt-3">${email}</p>
+  </div>
+</div>`
+  return card
+}
+
+for (let i = 0; i < teamMembers.length; i++) {
+  sectionArray += createCard(teamMembers[i])
+}
+
+cardSection.innerHTML = sectionArray
