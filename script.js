7891e4248d5df885d840e1eef6c1a93a114422eb
federicodececco <federicodececco1@gmail.com>
2024-12-13 17:09:56 +0100

    add: part of the from

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const formElm = document.getElementById('htmlForm')
+
+const cardSection = document.getElementById('teamCard')
+
+const createCard = struct => {
+  const { name, role, email, img } = struct
+  const card = `<div class="flex bg-black basis-[calc(33.333%-1rem)] ">
+  <div class="basis-1/3">
+    <img src="./${img}" alt="" />
+  </div>
+  <div class="p-3 text-white">
+    <h4 class="text-2xl font-semibold">${name}</h4>
+    <p class="pt-2">${role}</p>
+    <p class="email pt-3">${email}</p>
+  </div>
+</div>`
+  return card
+}
 const teamMembers = [
   {
     name: 'Marco Bianchi',
@@ -36,25 +54,30 @@ const teamMembers = [
     img: 'img/female3.png',
   },
 ]
-const cardSection = document.getElementById('teamCard')
+
 let sectionArray = ''
-const createCard = struct => {
-  const { name, role, email, img } = struct
-  const card = `<div class="flex bg-black basis-[calc(33.333%-1rem)] ">
-  <div class="basis-1/3">
-    <img src="./${img}" alt="" />
-  </div>
-  <div class="p-3 text-white">
-    <h4 class="text-2xl font-semibold">${name}</h4>
-    <p class="pt-2">${role}</p>
-    <p class="email pt-3">${email}</p>
-  </div>
-</div>`
-  return card
-}
 
 for (let i = 0; i < teamMembers.length; i++) {
   sectionArray += createCard(teamMembers[i])
 }
-
 cardSection.innerHTML = sectionArray
+
+formElm.addEventListener('submit', function (event) {
+  event.preventDefault()
+  const nameElm = document.getElementById('name')
+  const roleElm = document.getElementById('role')
+  const emailElm = document.getElementById('email')
+  const imgElm = document.getElementById('img')
+  const newMember = {
+    name: nameElm,
+    role: roleElm,
+    email: emailElm,
+    img: imgElm,
+  }
+  teamMembers.push(newMember)
+  sectionArray = ''
+  for (let i = 0; i < teamMembers.length; i++) {
+    sectionArray += createCard(teamMembers[i])
+  }
+  cardSection.innerHTML = sectionArray
+})
