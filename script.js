f411541dba7cabc819ae35284097c0a0d85c6d9b
federicodececco <federicodececco1@gmail.com>
2024-12-16 13:19:34 +0100

    feat: changed teamMember from array of arrays to array of classes

--- script.js
+++ script.js
@@ -2,57 +2,76 @@ const formElm = document.getElementById('htmlForm')
 
 const cardSection = document.getElementById('teamCard')
 
+class person {
+  #infos
+  constructor(name, role, email, img) {
+    this.#infos = [name, role, email, img]
+  }
+  getName() {
+    return this.#infos[0]
+  }
+  getRole() {
+    return this.#infos[2]
+  }
+  getEmail() {
+    return this.#infos[1]
+  }
+  getImg() {
+    return this.#infos[3]
+  }
+}
+
 const createCard = struct => {
   const { name, role, email, img } = struct
   const card = `<div class="flex bg:black dark:bg-slate-800  ">
   <div class="basis-1/3">
-    <img src="./${img}" alt="" />
+    <img src="./${struct.getImg()}" alt="" />
   </div>
   <div class="p-3 text-white dark:text-slate-100">
-    <h4 class="text-2xl font-semibold">${name}</h4>
-    <p class="pt-2">${role}</p>
-    <p class="email pt-3">${email}</p>
+    <h4 class="text-2xl font-semibold">${struct.getName()}</h4>
+    <p class="pt-2">${struct.getRole()}</p>
+    <p class="email pt-3">${struct.getEmail()}</p>
   </div>
 </div>`
   return card
 }
 const teamMembers = [
-  {
-    name: 'Marco Bianchi',
-    role: 'Designer',
-    email: 'marcobianchi@team.com',
-    img: 'img/male1.png',
-  },
-  {
-    name: 'Laura Rossi',
-    role: 'Front-end Developer',
-    email: 'laurarossi@team.com',
-    img: 'img/female1.png',
-  },
-  {
-    name: 'Giorgio Verdi',
-    role: 'Back-end Developer',
-    email: 'giorgioverdi@team.com',
-    img: 'img/male2.png',
-  },
-  {
-    name: 'Marta Ipsum',
-    role: 'SEO Specialist',
-    email: 'martarossi@team.com',
-    img: 'img/female2.png',
-  },
-  {
-    name: 'Roberto Lorem',
-    role: 'SEO Specialist',
-    email: 'robertolorem@team.com',
-    img: 'img/male3.png',
-  },
-  {
-    name: 'Daniela Amet',
-    role: 'Analyst',
-    email: 'danielaamet@team.com',
-    img: 'img/female3.png',
-  },
+  new person(
+    'Marco bianchi',
+    'marcobianchi@team.com',
+    'Designer',
+    'img/male1.png',
+  ),
+  new person(
+    'Laura Rossi',
+    'laurarossi@team.com',
+    'Front-end Developer',
+    'img/female1.png',
+  ),
+  new person(
+    'giorgio verdi',
+    'giorgioverdi@team.com',
+    'Back-end Developer',
+    'img/male2.png',
+  ),
+  new person(
+    'Marta Ipsum',
+    'martarossi@team.com',
+    'SEO Specialist',
+    'img/female2.png',
+  ),
+  new person(
+    'Roberto Lorem',
+    'robertolorem@team.com',
+    'SEO Specialist',
+    'img/male3.png',
+  ),
+  new person(
+    'Daniela Amet',
+    'danielaamet@team.com',
+    'Analyst',
+    'img/female3.png',
+  ),
 ]
 
 let sectionArray = ''
@@ -61,14 +80,15 @@ htmlPrint = teamMembers.map(createCard).join('')
 cardSection.innerHTML = htmlPrint
 formElm.addEventListener('submit', event => {
   event.preventDefault()
-  const newMember = {
-    name: document.getElementById('name').value,
-    role: document.getElementById('role').value,
-    email: document.getElementById('email').value,
-    img: document.getElementById('img').value,
-  }
-  teamMembers.push(newMember)
+  const newPerson = new person(
+    document.getElementById('name').value,
+    document.getElementById('role').value,
+    document.getElementById('email').value,
+    document.getElementById('img').value,
+  )
   sectionArray = ''
+  teamMembers.push(newPerson)
+
   sectionArray = teamMembers.map(createCard).join('')
 
   cardSection.innerHTML = sectionArray
